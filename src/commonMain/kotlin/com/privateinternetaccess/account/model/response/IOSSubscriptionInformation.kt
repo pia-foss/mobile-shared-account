cbfacc603736b3bf3e314857711cb163f7ee61b6
Juan Docal <juan@privateinternetaccess.com>
2021-02-05 14:01:31 +1100

    Update library

---
+++ src/commonMain/kotlin/com/privateinternetaccess/account/model/response/IOSSubscriptionInformation.kt
@@ -0,0 +1,53 @@
+/*
+ *  Copyright (c) 2020 Private Internet Access, Inc.
+ *
+ *  This file is part of the Private Internet Access Mobile Client.
+ *
+ *  The Private Internet Access Mobile Client is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  The Private Internet Access Mobile Client is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along with the Private
+ *  Internet Access Mobile Client.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.privateinternetaccess.account.model.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class IOSSubscriptionInformation(
+    @SerialName("available_products")
+    val availableProducts: List<AvailableProduct>,
+    @SerialName("eligible_for_trial")
+    val eligibleForTrial: Boolean,
+    @SerialName("receipt")
+    val receipt: Receipt,
+    @SerialName("status")
+    val status: String
+) {
+    @Serializable
+    data class AvailableProduct(
+        @SerialName("id")
+        val id: String,
+        @SerialName("legacy")
+        val legacy: Boolean,
+        @SerialName("plan")
+        val plan: String,
+        @SerialName("price")
+        val price: String
+    )
+
+    @Serializable
+    data class Receipt(
+        @SerialName("eligible_for_trial")
+        val eligibleForTrial: Boolean
+    )
+}
