9dbfe33d0deb7618c6500ccec865128ae786a680
Juan D <juan.docal@kape.com>
2023-10-03 14:53:12 +0200

    Update iOS KeychainSettings constructor to pass the accessible attribute

--- account/src/iosMain/kotlin/com/privateinternetaccess/account/internals/persistency/secureSettings/SecureSettingsProvider.kt
+++ account/src/iosMain/kotlin/com/privateinternetaccess/account/internals/persistency/secureSettings/SecureSettingsProvider.kt
@@ -1,12 +1,22 @@
 package com.privateinternetaccess.account.internals.persistency.secureSettings
 
+import com.russhwolf.settings.ExperimentalSettingsImplementation
 import com.russhwolf.settings.KeychainSettings
 import com.russhwolf.settings.Settings
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.Foundation.CFBridgingRetain
+import platform.Security.kSecAttrAccessible
+import platform.Security.kSecAttrAccessibleAlways
+import platform.Security.kSecAttrService
 
 internal actual object SecureSettingsProvider {
 
     private const val KEYCHAIN_NAME = "account_keychain"
 
+    @OptIn(ExperimentalSettingsImplementation::class, ExperimentalForeignApi::class)
     actual val settings: Settings?
-        get() = KeychainSettings(KEYCHAIN_NAME)
+        get() = KeychainSettings(
+            kSecAttrService to CFBridgingRetain(KEYCHAIN_NAME),
+            kSecAttrAccessible to kSecAttrAccessibleAlways
+        )
 }
